52798a93cf48245b8f5f6392665f0c507fd115ec
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-07-10 22:56:04 -0300

    add scroll top button

---
+++ script.js
@@ -0,0 +1,9 @@
+TopButton = document.getElementById('back-top-button')
+
+window.onscroll = function () {
+  scrollFunction()
+}
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
